73179ebc355e61bb1350cd06828c1a5ae1594c3c
André Leal <57924518+AndreLeal950@users.noreply.github.com>
2022-07-20 10:32:23 -0300

    Estlização e configuração das funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import { FiSearch } from 'react-icons/fi'
+import { useState } from 'react'
+import api from './services/api';
 
 import './styles/app.sass';
 
 function App() {
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+  
+  async function handleSearch() {
+
+    if (input === '') {
+      alert("Digite um CEP válido")
+    } else if (input.length > 8 || input.length < 8) {
+      alert("Digite um CEP válido")
+      
+      return;
+    }
+    try {
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput("")
+
+    } catch (error) {
+      alert("Erro ao buscar o CEP")
+      setInput("")
+      
+    }
+ }
+
   return (
     <div className="container">
       <h1 className='title'>Busca CEP</h1>
       <div className='containerInput'>
         <input
           type='text'
-          placeholder='Digite seu CEP...' />
+          placeholder='Digite seu CEP...'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}/>
         
-        <button className='buttonSearch'>
-          <FiSearch size={25} color='#FFF' />
+        <button className='buttonSearch' onClick={handleSearch}>
+          <FiSearch  size={25} color='#dacece'
+          />
         </button>
 
       </div>
