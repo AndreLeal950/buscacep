f7f0cf5e97d7388ced785d0a203d0f4d9aae0656
André Leal <57924518+AndreLeal950@users.noreply.github.com>
2022-07-20 08:50:28 -0300

    commit inicial, criação de uma página que busca o cep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi'
+
+import './styles/app.sass';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='title'>Busca CEP</h1>
+      <div className='containerInput'>
+        <input
+          type='text'
+          placeholder='Digite seu CEP...' />
+        
+        <button className='buttonSearch'>
+          <FiSearch size={25} color='#FFF' />
+        </button>
+
+      </div>
+
+        <main className='main'>
+          <h2>CEP: 00000-000</h2>
+          <spam>Rua: Teste algum</spam>
+          <spam>Complemento: Teste complemento</spam>
+          <spam>Bairro: Teste</spam>
+          <spam>Belém - PA</spam>
+        </main>
     </div>
   );
 }
